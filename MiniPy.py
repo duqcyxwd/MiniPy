e88afad44e88a1fdd3a6afe6acb6f3ed0c44b730
Yongqinchuan Du <duqcyxwd@gmail.com>
2014-11-20 20:10:57 -0500

    change accumolation variable start with 0

--- MiniPy.py
+++ MiniPy.py
@@ -19,7 +19,7 @@ class Minipy_evalCommand(sublime_plugin.TextCommand):
 
 		print("total = ", total)
 		# build a list from 1 to the number of special chars
-		serial_number = list(range(1, total+1))
+		serial_number = list(range(0, total+1))
 		# serial_number.reverse()
 		serial_number = rev(serial_number)
 		print(repr(serial_number))
